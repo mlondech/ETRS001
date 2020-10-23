7963c2306d16cf2f13779ba7f39907c65bf2a9b9
LONDECHAL <maxime.londechal@capgemini.com>
2020-10-23 18:37:16 +0200

    export json

--- Configuration/code/app.py
+++ Configuration/code/app.py
@@ -1,17 +1,18 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-
+from marshmallow_sqlalchemy.fields import Nested
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///configuration.db'
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 db = SQLAlchemy(app)
-
+ma = Marshmallow(app)
 admin = Admin(app, name='Configuration', template_mode='bootstrap3')
 
 Hardware_Monitoredobject = db.Table('Hardware_Monitoredobject',
@@ -23,10 +24,9 @@ class Monitoredobject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
     oid = db.Column(db.String(255), nullable=True)
-    
+
     def __repr__(self):
         return '<Monitored Object %r>' % self.name
-
 
 class Hardware(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,27 +37,35 @@ class Hardware(db.Model):
     ports = db.Column(db.Integer, nullable=True)
     community = db.Column(db.String(255), nullable=False)
 
-    monitoredobjects = db.relationship('Monitoredobject', secondary=Hardware_Monitoredobject, lazy='subquery',
-    backref=db.backref('hardwares', lazy=True))
+    monitoredobjects = db.relationship('Monitoredobject', secondary=Hardware_Monitoredobject,
+                                        lazy='subquery',backref=db.backref('hardwares', lazy=True))
 
     def __repr__(self):
         return '<Hardware %r>' % self.name
 
+class MonitoredobjectSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Monitoredobject
+
+class HardwareSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Hardware
+        include_relationships = True
+    monitoredobjects = Nested(MonitoredobjectSchema, many=True)
+
 admin.add_view(ModelView(Monitoredobject, db.session))
 admin.add_view(ModelView(Hardware, db.session))
+monitoredobjects_schema = MonitoredobjectSchema()
+hardwares_schema = HardwareSchema(many=True)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/config.html')
+@app.route('/json')
 def config():
-    return render_template('config.html')
-
-@app.route('/surv.html')
-def surv():
-    return render_template('surv.html')
-
+    hardwares = Hardware.query.all()
+    return jsonify(hardwares_schema.dump(hardwares))
 
 if __name__ == '__main__':
     app.run()
