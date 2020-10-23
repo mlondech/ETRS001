a2b3c04cd25035796d5aea40e40a7e210e3fa7aa
LONDECHAL <maxime.londechal@capgemini.com>
2020-10-23 13:45:08 +0200

    flask admin

--- Configuration/code/app.py
+++ Configuration/code/app.py
@@ -3,16 +3,52 @@
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from models import db
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////configuration.db'
-db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///configuration.db'
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+db = SQLAlchemy(app)
+
+admin = Admin(app, name='Configuration', template_mode='bootstrap3')
+
+Hardware_Monitoredobject = db.Table('Hardware_Monitoredobject',
+    db.Column('hardware_id', db.Integer, db.ForeignKey('hardware.id'), primary_key=True),
+    db.Column('monitoredobject_id', db.Integer, db.ForeignKey('monitoredobject.id'), primary_key=True)
+)
+
+class Monitoredobject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    oid = db.Column(db.String(255), nullable=True)
+    
+    def __repr__(self):
+        return '<Monitored Object %r>' % self.name
+
+
+class Hardware(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    description = db.Column(db.String(255), unique=True, nullable=True)
+    IPAddress = db.Column(db.String(255), unique=True, nullable=False)
+    IPmask = db.Column(db.String(255), nullable=False)
+    ports = db.Column(db.Integer, nullable=True)
+    community = db.Column(db.String(255), nullable=False)
+
+    monitoredobjects = db.relationship('Monitoredobject', secondary=Hardware_Monitoredobject, lazy='subquery',
+    backref=db.backref('hardwares', lazy=True))
+
+    def __repr__(self):
+        return '<Hardware %r>' % self.name
+
+admin.add_view(ModelView(Monitoredobject, db.session))
+admin.add_view(ModelView(Hardware, db.session))
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run()
--- Configuration/code/models.py
+++ Configuration/code/models.py
@@ -2,53 +2,55 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class MonitoredObject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    oid = db.Column(db.String(255), nullable=True)
+
+    # monitoredObjects = db.relationship('MonitoredObject', secondary=MonitoredObject_Group, lazy='subquery',
+    #     backref=db.backref('group', lazy=True))
+
+    def __repr__(self):
+        return '<Monitored Object %r>' % self.name
+        
 Hardware_MonitoredObject = db.Table('Hardware_MonitoredObject',
     db.Column('hardware_id', db.Integer, db.ForeignKey('hardware.id'), primary_key=True),
     db.Column('monitoredObject_id', db.Integer, db.ForeignKey('monitoredobject.id'), primary_key=True)
 )
 
-MonitoredObject_Group = db.Table('MonitoredObject_Group',
-    db.Column('monitoredObject_id', db.Integer, db.ForeignKey('monitoredobject.id'), primary_key=True),
-    db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True)
-)
+# MonitoredObject_Group = db.Table('MonitoredObject_Group',
+#     db.Column('monitoredObject_id', db.Integer, db.ForeignKey('monitoredobject.id'), primary_key=True),
+#     db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True)
+# )
 
-Group_Hardware = db.Table('Group_Hardware',
-    db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True),
-    db.Column('hardware_id', db.Integer, db.ForeignKey('hardware.id'), primary_key=True)
-)
+# Group_Hardware = db.Table('Group_Hardware',
+#     db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True),
+#     db.Column('hardware_id', db.Integer, db.ForeignKey('hardware.id'), primary_key=True)
+# )
 
 class Hardware(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
+    description = db.Column(db.String(255), unique=True, nullable=True)
     IPAddress = db.Column(db.String(255), unique=True, nullable=False)
     IPmask = db.Column(db.String(255), nullable=False)
     ports = db.Column(db.Integer, nullable=True)
     community = db.Column(db.String(255), nullable=False)
+    
+    # monitoredObjects = db.relationship('MonitoredObject', secondary=Hardware_MonitoredObject, lazy='subquery',
+    #     backref=db.backref('hardwares', lazy=True))
 
-    monitoredObjects = db.relationship('MonitoredObject', secondary=Hardware_MonitoredObject, lazy='subquery',
-        backref=db.backref('hardwares', lazy=True))
+    # groups = db.relationship('Groups', secondary=Group_Hardware, lazy='subquery',
+    #     backref=db.backref('hardwares', lazy=True))
 
-    groups = db.relationship('Groups', secondary=Group_Hardware, lazy='subquery',
-        backref=db.backref('hardwares', lazy=True))
-
+    # hw = Hardware(name='e', description='x',IPAddress='j',IPmask='r',community='h')
     def __repr__(self):
-        return '<Hardware %r>' % self.username
+        return '<Hardware %r>' % self.name
 
-class MonitoredObject(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True, nullable=False)
-    oid = db.Column(db.String(255), unique=True, nullable=False)
 
-    monitoredObjects = db.relationship('MonitoredObject', secondary=MonitoredObject_Group, lazy='subquery',
-        backref=db.backref('group', lazy=True))
+# class Group(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(255), unique=True, nullable=False)
 
-    def __repr__(self):
-        return '<Monitored Object %r>' % self.name
-
-class Group(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<Group %r>' % self.name
-
+#     def __repr__(self):
+#         return '<Group %r>' % self.name
